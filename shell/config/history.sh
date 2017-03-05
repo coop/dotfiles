d91296c8d95556bdfb0467ad0aac22ecaa95237f
Tim Cooper <tim@timcorp.io>
2017-03-05 10:01:12 +1000

    Update bash history settings
    See https://github.com/mrzool/bash-sensible/blob/master/sensible.bash

--- shell/config/history.sh
+++ shell/config/history.sh
@@ -6,9 +6,20 @@ HISTCONTROL=ignoreboth
 shopt -s histappend
 
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
-HISTSIZE=9000
-HISTFILESIZE=$HISTSIZE
+HISTSIZE=500000
+HISTFILESIZE=100000
 HISTFILE="${XDG_CONFIG_HOME}/bash/history"
+
+# Avoid duplicate entries
+HISTCONTROL="erasedups:ignoreboth"
+
+# Don't record some commands
+export HISTIGNORE="&:[ ]*:exit:bg:fg:history:clear"
+
+# Use standard ISO 8601 timestamp
+# %F equivalent to %Y-%m-%d
+# %T equivalent to %H:%M:%S (24-hours format)
+HISTTIMEFORMAT='%F %T '
 
 if [[ ! -d "${XDG_CONFIG_HOME}/bash" ]]; then
   mkdir -p "${XDG_CONFIG_HOME}/bash"
@@ -21,7 +32,7 @@ history() {
 
 _bash_history_sync() {
   builtin history -a
-  HISTFILESIZE=$HISTSIZE
+  HISTFILESIZE=100000
   builtin history -c
   builtin history -r
 }
