c534c49cb17342e0add4e6701711a4447f5134a9
Tim Cooper <tim@timcorp.io>
2017-03-10 16:50:35 +1000

    Add nodejs keyring when installing

--- shell/config/asdf.d/nodejs.sh
+++ shell/config/asdf.d/nodejs.sh
@@ -1,3 +1,4 @@
 if ! asdf plugin-list | grep -q nodejs; then
   asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+  bash "${XDG_DATA_HOME}/asdf/plugins/nodejs/bin/import-release-team-keyring"
 fi
